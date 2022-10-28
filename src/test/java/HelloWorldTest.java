e353293e38f1012c24ae3d675eafa9f2c2c54d2a
Marat <marat1989.ne@gmail.com>
2022-10-28 20:34:57 +0300

    Hello World Test

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 public class HelloWorldTest {
 
     @Test
-    public void HelloWorld(){
+    public void testHelloWorld(){
         System.out.println("hello");
     }
 }
